8dd9f6066e8ea12b6943bfa28844c95408d4888d
gdlongmao <gdlongmao89@gmail.com>
2020-10-16 07:11:24 +0800

    React Route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {Route, Switch} from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/homepage/homepage";
 
-class App extends Component {
-  render() {
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
+
+const TopicsList = () => {
+  return(
+    <div>
+      <h1>Topics List</h1>
+    </div>
+  );
+};
+
+const TopicDetail = () => {
+  return(
+    <div>
+      <h1>Topic Detail</h1>
+    </div>
+  );
+};
+
+
+function App() {
     return (
-    <div className='App'>
-      <HomePage />
+    <div>
+      <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/topics" component={TopicsList} />
+      <Route exact path="/topics/new" component={TopicsList} />
+      <Route exact path="/topics/:topicId" component={TopicDetail} />
+      </Switch>
     </div>
     );
-  }
-}
+ };
 
 export default App;
