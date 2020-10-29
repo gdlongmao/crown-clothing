cc4f28d558eca98e89243e8acd3e6d3513303fc1
gdlongmao <gdlongmao89@gmail.com>
2020-10-29 23:19:19 +0800

    lesson 186 container pattern

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,14 +1,14 @@
 import ShopActionTypes from "./shop.types";
-import {firestore, convertCollectionsSnapshotToMap}
+import { firestore, convertCollectionsSnapshotToMap } from "../../firebase/firebase.utils";
 
 export const fetchCollectionsStart = () => ({
     type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
 
-export const fetchCollectionsSuccess = collectionsMap => {
-  type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
-  payload: collectionsMap
-}
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+})
 
 export const fetchCollectionsFailure = errorMessage => ({
     type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
@@ -21,11 +21,11 @@ export const fetchCollectionsStartAsync = () => {
         dispatch(fetchCollectionsStart());
 
         collectionRef
-        .get().
-        then(snapshot => {
-            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-            dispatch(fetchCollectionsSuccess(collectionsMap));
-        })
-        .catch(error => dispatch(fetchCollectionsFailure(error.message)));
+            .get().
+            then(snapshot => {
+                const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+                dispatch(fetchCollectionsSuccess(collectionsMap));
+            })
+            .catch(error => dispatch(fetchCollectionsFailure(error.message)));
     };
 };
