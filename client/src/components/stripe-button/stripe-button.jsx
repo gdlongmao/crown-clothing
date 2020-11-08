a4477cfffc4464cc4aea04523a201695fec350a7
gdlongmao <gdlongmao89@gmail.com>
2020-11-08 23:13:14 +0800

    lesson 227 Deploying To Production

--- client/src/components/stripe-button/stripe-button.jsx
+++ client/src/components/stripe-button/stripe-button.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+import axios from "axios";
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = "pk_test_51HgQHOALNT4ZqHpLXRFbQ4vHYo9dcAKPbrpbW4okB5sN0auRExtXYBNl75mpInuj1Lm1N8bZ0qNJVaGqR9PBHC7F00FCEq6YS9"
 
     const onToken = token => {
-        console.log(token);
-        alert("Payment Successful");
+      axios({
+          url: "payment",
+          method: "post",
+          data: {
+              amount: priceForStripe,
+              token
+          }
+      }).then(response => {
+          alert("Payment successful!")
+      }).catch(error => {
+          console.log("Payment error: ", JSON.parse(error));
+          alert("There was an issue with your payment. Please make sure you use the provided credit card.")
+      })
     }
 
     return (
